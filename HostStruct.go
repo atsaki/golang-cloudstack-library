1f82cd9c571322fe0259570ba65d7659b4870d37
Atsushi Sasaki <atsaki01@gmail.com>
2016-07-26 00:43:34 +0900

    merge HostStruct.go and DedicatedHostStruct.go

--- HostStruct.go
+++ HostStruct.go
@@ -132,3 +132,19 @@ type HostTag struct {
 	// the name of the host tag
 	Name NullString `json:"name"`
 }
+
+type DedicatedHost struct {
+	ResourceBase
+	// the Account ID of the host
+	AccountId ID `json:"accountid"`
+	// the Dedication Affinity Group ID of the host
+	AffinityGroupId ID `json:"affinitygroupid"`
+	// the domain ID of the host
+	DomainId ID `json:"domainid"`
+	// the ID of the host
+	HostId ID `json:"hostid"`
+	// the name of the host
+	HostName NullString `json:"hostname"`
+	// the ID of the dedicated resource
+	Id ID `json:"id"`
+}
