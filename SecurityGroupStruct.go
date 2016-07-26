5ea8cbe83074ee5ef62dc56e1a9dbb1c45e9a659
Atsushi Sasaki <atsaki01@gmail.com>
2016-07-26 14:26:07 +0900

    merge securitygroup structs

--- SecurityGroupStruct.go
+++ SecurityGroupStruct.go
@@ -25,3 +25,51 @@ type SecurityGroup struct {
 	// the list of resource tags associated with the rule
 	Tags []Tag `json:"tags"`
 }
+
+type SecurityGroupIngress struct {
+	ResourceBase
+	// account owning the security group rule
+	Account NullString `json:"account"`
+	// the CIDR notation for the base IP address of the security group rule
+	Cidr NullString `json:"cidr"`
+	// the ending IP of the security group rule
+	EndPort NullNumber `json:"endport"`
+	// the code for the ICMP message response
+	IcmpCode NullNumber `json:"icmpcode"`
+	// the type of the ICMP message response
+	IcmpType NullNumber `json:"icmptype"`
+	// the protocol of the security group rule
+	Protocol NullString `json:"protocol"`
+	// the id of the security group rule
+	RuleId ID `json:"ruleid"`
+	// security group name
+	SecurityGroupName NullString `json:"securitygroupname"`
+	// the starting IP of the security group rule
+	StartPort NullNumber `json:"startport"`
+	// the list of resource tags associated with the rule
+	Tags []Tag `json:"tags"`
+}
+
+type SecurityGroupEgress struct {
+	ResourceBase
+	// account owning the security group rule
+	Account NullString `json:"account"`
+	// the CIDR notation for the base IP address of the security group rule
+	Cidr NullString `json:"cidr"`
+	// the ending IP of the security group rule
+	EndPort NullNumber `json:"endport"`
+	// the code for the ICMP message response
+	IcmpCode NullNumber `json:"icmpcode"`
+	// the type of the ICMP message response
+	IcmpType NullNumber `json:"icmptype"`
+	// the protocol of the security group rule
+	Protocol NullString `json:"protocol"`
+	// the id of the security group rule
+	RuleId ID `json:"ruleid"`
+	// security group name
+	SecurityGroupName NullString `json:"securitygroupname"`
+	// the starting IP of the security group rule
+	StartPort NullNumber `json:"startport"`
+	// the list of resource tags associated with the rule
+	Tags []Tag `json:"tags"`
+}
